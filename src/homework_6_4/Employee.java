69021d5db753a1c58cd2782c8518fdb7a64274fd
Amid-1 <amid.1spbtj@gmail.com.com>
2024-09-27 20:28:58 +0300

    new classes

---
+++ src/homework_6_4/Employee.java
@@ -0,0 +1,5 @@
+package homework_6_4;
+
+public interface Employee {
+    double getMonthSalary();
+}
---
+++ src/homework_6_4/Manager.java
@@ -0,0 +1,13 @@
+package homework_6_4;
+
+public class Manager implements Employee{
+    private static final double FIXED_SALARY = 50000;
+    private double bonus;
+
+    
+
+    @Override
+    public double getMonthSalary() {
+        return 0;
+    }
+}
